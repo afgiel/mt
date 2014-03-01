1d436f0a5c0707c6cd1676737016f4261b15fdcf
jnecamp <jnecamp5@gmail.com>
2014-02-28 16:44:22 -0800

    Final code base

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 CS124 Machine Translation Assignment
+
+The main module can be found in translation.py.  To execute
+translation system, simply call "python translation.py"
--- src/test.py
+++
@@ -1,6 +0,0 @@
-import implicitSubject
-
-sentence = [['jim'], ['<IT> walks', '<IT> talks'], ['alone']]
-tags = ['heheheh','VERB', 'ADJ']
-implicitSubject.removeTags(sentence, tags)
-print sentence
